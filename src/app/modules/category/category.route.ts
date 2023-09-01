28c8532c1ce697f7436acfa2f19c8d9698802a58
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-01 22:18:29 +0600

    [DONE] Create and Update Category Validation Added

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { CategoryController } from './category.controller';
+import { CategoryValidation } from './category.validation';
 
 const router = express.Router();
 
-router.post('/create-category', CategoryController.createCategory);
+router.post(
+  '/create-category',
+  validateRequest(CategoryValidation.createCategory),
+  CategoryController.createCategory
+);
 
 router.get('/', CategoryController.getAllCategories);
 
 router.get('/:id', CategoryController.getSingleCategory);
 
-router.patch('/:id', CategoryController.updateCategory);
+router.patch(
+  '/:id',
+  validateRequest(CategoryValidation.updateCategory),
+  CategoryController.updateCategory
+);
 
 router.delete('/:id', CategoryController.deleteCategory);
 
---
+++ src/app/modules/category/category.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const createCategory = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+const updateCategory = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+export const CategoryValidation = {
+  createCategory,
+  updateCategory,
+};
