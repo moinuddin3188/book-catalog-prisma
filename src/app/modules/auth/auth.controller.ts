8a5bb2b09252f9ab9be95d5971cb83ab864ff6b7
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-01 11:26:57 +0600

    [DONE] Simple user with Access Token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { User } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import config from '../../../config';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
+import { ILoginUserResponse } from './auth.interface';
 import { AuthService } from './auth.service';
 
 const userSignUp = catchAsync(async (req: Request, res: Response) => {
@@ -10,7 +12,7 @@ const userSignUp = catchAsync(async (req: Request, res: Response) => {
 
   const result = await AuthService.userSignUp(user);
 
-  sendResponse<User>(res, {
+  sendResponse<Omit<User, 'password'>>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User signed up successfully!',
@@ -18,6 +20,25 @@ const userSignUp = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const userLogin = catchAsync(async (req: Request, res: Response) => {
+  const { ...loginData } = req.body;
+
+  const { refreshToken, ...others } = await AuthService.userLogin(loginData);
+
+  //set refresh token into cookie
+  const cookieOptions = { secure: config.env === 'production', httpOnly: true };
+
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+
+  sendResponse<ILoginUserResponse>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User signed up successfully!',
+    data: others,
+  });
+});
+
 export const AuthController = {
   userSignUp,
+  userLogin,
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,26 @@
 import { User } from '@prisma/client';
 import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
 import prisma from '../../../shared/prisma';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
 
-const userSignUp = async (userData: User): Promise<User> => {
+const userSignUp = async (userData: User): Promise<Omit<User, 'password'>> => {
   const newUser = await prisma.user.create({
     data: userData,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      contactNo: true,
+      address: true,
+      role: true,
+      createdAt: true,
+      updatedAt: true,
+      profileImg: true,
+    },
   });
 
   if (!newUser) {
@@ -15,6 +30,54 @@ const userSignUp = async (userData: User): Promise<User> => {
   return newUser;
 };
 
+const userLogin = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
+  const { email: userEmail, password } = payload;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email: userEmail,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, "User doesn't exist");
+  }
+
+  if (isUserExist.password && password !== isUserExist.password) {
+    throw new ApiError(
+      httpStatus.UNAUTHORIZED,
+      'Password or email is not correct'
+    );
+  }
+
+  // create access token and refresh token
+  const { email, role } = isUserExist;
+
+  const accessToken = jwtHelpers.createToken(
+    {
+      email,
+      role,
+    },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = jwtHelpers.createToken(
+    {
+      email,
+      role,
+    },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
 export const AuthService = {
   userSignUp,
+  userLogin,
 };
