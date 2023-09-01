4fd72c12c47360660b4fca626ac5b7a6db28b947
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-01 22:11:33 +0600

    [DONE] Create and Update Book Validation Added

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { BookController } from './book.controller';
+import { BookValidation } from './book.validation';
 
 const router = express.Router();
 
-router.post('/create-book', BookController.createBook);
+router.post(
+  '/create-book',
+  validateRequest(BookValidation.createBook),
+  BookController.createBook
+);
 
 router.get('/', BookController.getAllBook);
 
 router.get('/:id', BookController.getSingleBook);
 
-router.patch('/:id', BookController.updateBook);
+router.patch(
+  '/:id',
+  validateRequest(BookValidation.updateBook),
+  BookController.updateBook
+);
 
 router.delete('/:id', BookController.deleteBook);
 
