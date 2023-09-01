a6482e787bbf371081058fc8861a3c495ff77633
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-01 22:58:30 +0600

    [DONE] Create and Update User Validation Added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllUser);
+router.get(
+  '/',
+  validateRequest(UserValidation.createUser),
+  UserController.getAllUser
+);
 
 router.get('/:id', UserController.getSingleUser);
 
-router.patch('/:id', UserController.updateUser);
+router.patch(
+  '/:id',
+  validateRequest(UserValidation.updateUser),
+  UserController.updateUser
+);
 
 router.delete('/:id', UserController.updateUser);
 
