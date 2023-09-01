077d27d2e423cfc1862ca5a4e787899aa5b2b63e
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-01 09:20:30 +0600

    first commit

---
+++ prisma/migrations/20230901031123_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "role" AS ENUM ('admin', 'customer');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "role" NOT NULL,
+    "contactNo" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "profileImg" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
