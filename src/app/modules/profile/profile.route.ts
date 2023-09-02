4de8fd3e27443a8f0e5b10bd8b150ffcf33ea726
moinuddin3188 <moinuddin3188@gmail.com>
2023-09-02 11:09:43 +0600

    [DONE] Added auth guard for all routes

--- src/app/modules/profile/profile.route.ts
+++ src/app/modules/profile/profile.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { ProfileController } from './profile.controller';
 
 const router = express.Router();
 
-router.get('/', ProfileController.getSingleUser);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.CUSTOMER),
+  ProfileController.getSingleUser
+);
 
 export const ProfileRoutes = router;
